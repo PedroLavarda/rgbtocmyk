6cba4e28a15033fa608939976ea56e7f7ef86928
Pedro <PedroLavarda>
2025-09-18 19:24:20 -0300

    reverso

--- main.py
+++ main.py
@@ -10,8 +10,24 @@ def converter_rgb_para_cmyk(caminho_imagem_entrada, caminho_imagem_saida):
     except Exception as e:
         print(f"Ocorreu um erro durante a conversão: {e}")
 
+def converter_cmyk_pra_rgb(caminho_imagem_entrada, caminho_imagem_saida):
+    try:
+        imagem_cmyk = Image.open(caminho_imagem_entrada)
+        imagem_rgb = imagem_cmyk.convert('RGB')
+        imagem_rgb.save(caminho_imagem_saida)
+
+        print(f"Imagem convertida com sucesso e salva em: {caminho_imagem_saida}")
+    except Exception as e:
+        print(f"Ocorreu um erro durante a conversão: {e}")
+
+
 
 caminho_entrada = r'/home/pedro/Documents/opob/rgbtocmyk/aurora.jpg'
 caminho_saida = r'/home/pedro/Documents/opob/rgbtocmyk/auroracmyk.jpg'
 
+
+# caminho_entrada = r'/home/pedro/Documents/opob/rgbtocmyk/auroracmyk.jpg'
+# caminho_saida = r'/home/pedro/Documents/opob/rgbtocmyk/aurora2.jpg'
+
 converter_rgb_para_cmyk(caminho_entrada, caminho_saida)
+# converter_cmyk_pra_rgb(caminho_entrada, caminho_saida)
